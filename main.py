65fc4c5b4867888aae66de13b19aceb9609999bf
Dante0214 <rn_chic@naver.com>
2025-06-30 13:10:48 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print('hello mars')
+try:
+    with open("C:/Users/USER/Desktop/Proj/SC/mission_computer_main.log",'r',encoding='utf-8') as f:
+       for line in f:
+         print(line.rstrip())
+except FileNotFoundError as e:
+   print(e)
+except PermissionError as e:
+   print(e)
+except UnicodeDecodeError as e:
+   print(e)
+except IOError as e:
+   print(e)
+
